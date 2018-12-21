6b24386e7b3d07eeededcc832c56458165d1ddaa
William Dunk <williamdunk@gmail.com>
2018-12-21 19:11:51 +0000

    added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-#student array
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -17,21 +16,46 @@ def input_students
   #return the array of students
   students
 end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 def print(students)
   students.each_with_index do |student, idx|
     student_count = idx + 1
     puts "#{student_count}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
-#nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
